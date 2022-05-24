1f594d61f24a71092bc8c68b75ed9eb347e69f3b
Same Mahmoud <samemahmoud24@gmail.com>
2022-05-24 14:35:20 +0200

    Added validations to boat

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,7 @@ class Boat < ApplicationRecord
   has_one_attached :image
 
   validates :image, presence: true
+  validates :price, numericality: { other_than: 0 }
+  validates :size, numericality: { other_than: 0 }
+  validates :description, length: { minimum: 5 }
 end
