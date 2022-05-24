e11ca414bd39a08c32c9f39dd1c40f0b18860d7e
eljopleqi <eljopleqi@gmail.com>
2022-05-24 15:13:35 +0200

    small typos fixed

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,14 +3,14 @@ class Boat < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :image
 
-  validates :photo, presence: true
+  validates :image, presence: true
   validates :title, presence: true
   validates :image, presence: true
 
   validates :city, presence: true
   validates :address, presence: true
-  validates :price, presence: true ,numericality: { other_than: 0 }
-  validates :size, presence: true ,numericality: { other_than: 0 }
-  validates ,:description, presence: true ,length: { minimum: 5 }
-  
+  validates :price, presence: true, numericality: { other_than: 0 }
+  validates :size, presence: true, numericality: { other_than: 0 }
+  validates :description, presence: true, length: { minimum: 5 }
+
 end
