7b5654293aae677872b548a405d352ed29404c04
eljopleqi <eljopleqi@gmail.com>
2022-05-24 10:08:37 +0200

    fixed the bug where boat entries where not created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:home, :boats]
 
-  # before_action :authenticate_user!
-  # skip_before_action :authenticate_user!, only: :home
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # add_flash_types :booking
+  add_flash_types :booking
 
   def configure_permitted_parameters
-      # For additional fields in app/views/devise/registrations/new.html.erb
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :owner])
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :owner])
   end
 end
