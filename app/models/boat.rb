62ad8a72233f8ae87c48c867d329cc4d47655a2b
Same Mahmoud <samemahmoud24@gmail.com>
2022-05-24 11:37:07 +0200

    Added validations to models

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -3,4 +3,11 @@ class Boat < ApplicationRecord
   has_one_attached :photo
 
   validates :photo, presence: true
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :price, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :size, presence: true
+  validates :description, presence: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
+  validates :check_in, presence: true
+  validates :check_out, presence: true
 end
